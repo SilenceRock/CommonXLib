a7ce782205a2e0c752c0a31e5a2133024099f27d
yanghang <yanghang@cyclecentury.com>
2023-01-05 16:25:02 +0800

    1.删除timber本地jar包，改为远程依赖

---
+++ app/src/main/java/com/sr/commonlib/Constant.kt
@@ -0,0 +1,11 @@
+package com.sr.commonlib
+
+/**
+ * FileName: Constant
+ * Author: hangY
+ * Date: 2022/5/30 15:03
+ * Description:
+ */
+object Constant {
+
+}
--- app/src/main/java/com/sr/commonlib/MainActivity.kt
+++ app/src/main/java/com/sr/commonlib/MainActivity.kt
@@ -3,14 +3,13 @@ package com.sr.commonlib
 import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.os.Bundle
-import android.os.Handler
-import android.os.Process
-import android.os.SystemClock
+import android.os.CountDownTimer
 import com.sr.superhelperx.activity.AppKtV4Activity
-import com.sr.superhelperx.app.AppApplication.INSTANCE
-import com.sr.superhelperx.app.AppInterface
 import com.sr.superhelperx.util.UtilLog
 import com.sr.superhelperx.util.UtilToast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.InetAddress
+import kotlin.concurrent.thread
 import kotlin.system.exitProcess
 
 class MainActivity : AppKtV4Activity() {
@@ -26,11 +25,39 @@ class MainActivity : AppKtV4Activity() {
 
         initHandlerCallBack()
 //        GlideLoader.getInstance()[context,"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftp03%2F1Z921104Z92S8-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1651802443&t=1c07622a3b11f0632d570cd633a0733d",img_test]
+
+//        parsingDomain("skmobiletv.96396.cn")
+
+        tv_hello.setOnClickListener {
+//            ll_item.removeView(img_test)
+        }
+
+        tv_hello_world.setOnClickListener {
+//            ll_item.addView(img_test,2)
+        }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         UtilLog.e(TAG, "onConfigurationChanged fontScale = " + newConfig.fontScale)
         super.onConfigurationChanged(newConfig)
+    }
+
+    private fun parsingDomain(hostUrl: String){
+        var ipAddress = ""
+        thread {
+            try {
+                val mInetAddress = InetAddress.getByName(hostUrl)
+                if (mInetAddress != null){
+                    ipAddress = mInetAddress.hostAddress
+                }
+                UtilLog.e(TAG,"ipAddress = $ipAddress")
+                InetAddress.getAllByName(hostUrl).forEach {
+                    UtilLog.e(TAG,"hostAddress = ${it.hostAddress}")
+                }
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun initHandlerCallBack(){
